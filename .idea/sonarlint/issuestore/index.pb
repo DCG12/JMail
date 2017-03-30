bce5165ef48c80a10b7ef63389042778a3ce568f
David <46406163y@iespoblenou.org>
2017-03-30 18:49:59 +0200

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+I
+src/com/company/Main.java,4/a/4a831b10439099d44774d0ccc387e808deffaeab
+Z
+*src/com/company/SendAttachmentInEmail.java,6/7/67e2bb3490fcc0711764556fab057454511fefe5
+P
+ src/com/company/SendHotmail.java,7/8/7819c74c6c06aa2105b6b6274d383112ac16337f
---
+++ src/com/company/SendAttachmentInEmail.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
+
+public class SendAttachmentInEmail {
+
+    /*
+    ESTA CLASE ENVIA UN EMAIL CON UN ARCHIVO ADJUNTO EN GMAIL
+    EL ARCHIVO ADJUNTO ES UN PNG LLAMADO INDEX.PNG
+     */
+
+    public static void main(String[] args) {
+
+
+        //Clase para enviar un email junto con un archivo
+        final String username = "odio-1@hotmail.com";
+        final String password = "theking12";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.live.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("odio-1@hotmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("mispruebas434@gmail.com"));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Now set the actual message
+            messageBodyPart.setText("This is message body");
+
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "src/index.png";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            message.setContent(multipart);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
